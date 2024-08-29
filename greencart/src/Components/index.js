9cfb5da631f9547081e1ab7495f580c4a8d0c708
ulaganathan001 <147390021+ulaganathan001@users.noreply.github.com>
2024-08-29 13:04:57 +0530

    Delete greencart directory

--- greencart/src/Components/index.js
+++
@@ -1,8 +0,0 @@
-import Delivery from "./Delivery";
-import Healthy from "./Healthy";
-import Hero from "./Hero";
-import HProduct from "./HProduct";
-import Navbar from "./Navbar";
-import Bestseller from "./Bestseller";
-import Reviews from "./Reviews";
-export { Delivery,Healthy,Hero,HProduct,Navbar,Bestseller,Reviews};
